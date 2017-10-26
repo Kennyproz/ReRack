34f5812761fa051d36beb50171b62111c2aadf79
Kennyproz <k.odigwegomez@student.fontys.nl>
2017-10-26 13:28:34 +0200

    change

--- app/src/main/java/com/example/ken/rerack/Login/MainActivity.java
+++ app/src/main/java/com/example/ken/rerack/Login/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
